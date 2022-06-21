d126cd4595cbe225bc8e2dfddda5245d3a79533c
VictorC1011 <105716896+VictorC1011@users.noreply.github.com>
2022-06-21 05:34:48 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,37 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Jair Messias Bolsonaro',
+                partido: 'Partido Liberal',
+                fotos:[
+                    {url: 'bolsonaro.jpeg', legenda: 'Presidente'},
+                    {url: 'tereza.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+
+            {
+                numero: '13',
+                nome: 'Luiz Inácio Lula da Silva',
+                partido: 'Partido dos Trabalhadores',
+                fotos:[
+                    {url: 'lula.jpg', legenda: 'Presidente'},
+                    {url: 'alckmin.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+
+            {
+                numero: '12',
+                nome: 'Ciro Gomes',
+                partido: 'Partido Democrático Trabalhista',
+                fotos:[
+                    {url: 'ciro.jpg', legenda: 'Presidente'},
+                    {url: 'suely.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+    }
+]
